d3406b80e202691cac5ff835f39d522c265e8007
David Harris <32494929+georgeC0stanza@users.noreply.github.com>
2020-05-12 15:57:24 -0600

    major update plus some experimentalness that has to be tested online?

--- main.js
+++ main.js
@@ -3,262 +3,186 @@
 const TILEMAPSIZE = 160; // Total number of tiles displayed. Must be a multiple of TILECOLUMNS.
 const TILECOLUMNS = 16; // Number of columns
 
-let tileSet = []; // Set of all tiles to be used
-let tilemapList = []; // The order in which the tiles are used via references to elements in tileSet
-let spriteSet = []; // Set of all sprites to be used
-
-loadResources(); 
-
-let sprites = (function (){
-    function createSprites(spritesList) {
-        let spriteCount = 0;
-        spritesList.forEach(x => {
-            let sprite = document.createElement('img');
-            sprite.src = x;
-            sprite.className = "sprites";
-            sprite.id = "sprite" + spriteCount;
-            sprite.style.visibility = "hidden";
-            ++spriteCount;
-            document.getElementById("window").appendChild(sprite);
+class SpritesMaker {
+    constructor(spritesList) {
+        const main = document.querySelector('.main');
+        this.sprites = spritesList.map(srcStr => {
+            const sprite = document.createElement('img');
+            sprite.src = srcStr;
+            main.appendChild(sprite);
+            return sprite;
         });
     }
 
-    function show(index) {
-        const spriteStyle = document.getElementById("sprite" + index).style
-        spriteStyle.visibility = "visible";
+    show(index) {
+        this.sprites[index].style.visibility = 'visible';
     }
 
-    function hide(index) {
-        spriteStyle = document.getElementById("sprite" + index).style
-        spriteStyle.visibility = "hidden";
+    hide(index) {
+        this.sprites[index].style.visibility = 'hidden';
     }
-    
-    function reflectY(spriteIndex) {
-        const sprite = "sprite" + spriteIndex;
-        const transform = document.getElementById(sprite).style.transform;
+
+    reflectY(index) {
+        const { style } = this.sprites[index];
+        const transform = style.transform;
         if (transform === "scale(-1, 1)"){ 
-            document.getElementById(sprite).style.transform = "scale(1)";
+            style.transform = "scale(1)";
         }
         else if (transform === "scale(1, -1)"){
-            document.getElementById(sprite).style.transform = "scale(-1)";
+            style.transform = "scale(-1)";
         }
         else if (transform === "scale(-1)"){
-            document.getElementById(sprite).style.transform = "scale(1, -1)";
+            style.transform = "scale(1, -1)";
         }
         else {
-            document.getElementById(sprite).style.transform = "scale(-1, 1)";
+            style.transform = "scale(-1, 1)";
         }
-    }
-    
-    function reflectX(spriteIndex) {
-        const sprite = "sprite" + spriteIndex;
-        const transform = document.getElementById(sprite).style.transform;
-        if (transform === "scale(-1, 1)"){
-            document.getElementById(sprite).style.transform = "scale(-1)";
-        }
-        else if (transform === "scale(1, -1)"){
-            document.getElementById(sprite).style.transform = "scale(1)";
-        }
-        else if (transform === "scale(-1)"){
-            document.getElementById(sprite).style.transform = "scale(-1, 1)";
-        }
-        else {
-            document.getElementById(sprite).style.transform = "scale(1, -1)";
-        }
-    }
-    
-    function reflect(spriteIndex, dX, dY) {
-        const sprite = "sprite" + spriteIndex;
-        if (dX === true && dY === true){
-            document.getElementById(sprite).style.transform = "scale(1)";
-        }
-        else if (dX === true && dY === false){
-            document.getElementById(sprite).style.transform = "scale(1, -1)";
-        }
-        else if (dX === false && dY === true){
-            document.getElementById(sprite).style.transform = "scale(-1, 1)";
-        }
-        else if (dX === false && dY === false){
-            document.getElementById(sprite).style.transform = "scale(-1)";
-        }
-    }
-    
-    function translate(spriteIndex, dX, dY) {
-        const sprite = "sprite" + spriteIndex;
-        const posLeft = parseInt(document.getElementById(sprite).style.left.slice(0, -2)) + dX;
-        const posTop = parseInt(document.getElementById(sprite).style.top.slice(0, -2)) + dY;
-        document.getElementById(sprite).style.left = posLeft + "px";
-        document.getElementById(sprite).style.top = posTop + "px";
-    }
-    
-    function setPosition(spriteIndex, dX, dY) {
-        const sprite = "sprite" + spriteIndex;
-        document.getElementById(sprite).style.left = dX +"px";
-        document.getElementById(sprite).style.top = dY + "px";
     }
 
-    function getPosition(spriteIndex) {
-        const sprite = "sprite" + spriteIndex;
+    reflectX(index) {
+        const { style } = this.sprites[index];
+        const transform = style.transform;
+        if (transform === "scale(-1, 1)"){
+            style.transform = "scale(-1)";
+        }
+        else if (transform === "scale(1, -1)"){
+            style.transform = "scale(1)";
+        }
+        else if (transform === "scale(-1)"){
+            style.transform = "scale(-1, 1)";
+        }
+        else {
+            style.transform = "scale(1, -1)";
+        }
+    }
+
+    reflect(index, dX, dY) {
+        const { style } = this.sprites[index];
+        if (dX && dY){
+            style.transform = "scale(1)";
+        }
+        else if (dX && !dY){
+            style.transform = "scale(1, -1)";
+        }
+        else if (!dX && dY){
+            style.transform = "scale(-1, 1)";
+        }
+        else if (!dX && !dY){
+            style.transform = "scale(-1)";
+        }
+    }
+
+    translate(index, dX, dY) {
+        const { style } = this.sprites[index];
+        const posLeft = Number(style.left.slice(0, -2)) + dX;
+        const posTop = Number(style.top.slice(0, -2)) + dY;
+        style.left = posLeft + "px";
+        style.top = posTop + "px";
+    }
+
+    setPosition(index, dX, dY) {
+        const { style } = this.sprites[index];
+        style.left = dX +"px";
+        style.top = dY + "px";
+    }
+
+    getPosition(index) {
+        const { style } = this.sprites[index];
         return {
-            x: parseInt(document.getElementById(sprite).style.left.slice(0, -2)),
-            y: parseInt(document.getElementById(sprite).style.top.slice(0, -2))
+            x: Number(style.left.slice(0, -2)),
+            y: Number(style.top.slice(0, -2))
         };
     }
 
-    function getOrientation(spriteIndex) {
-        const sprite = "sprite" + spriteIndex;
-        const transform = document.getElementById(sprite).style.transform;
-        let pair = {};
-
+    getOrientation(index) {
+        const { transform } = this.sprites[index].transform;
         if (transform === "scale(1)"){
-            pair = {x: 1, y: 1};
+            return {x: 1, y: 1};
         }
         else if (transform === "scale(-1)"){
-            pair = {x: -1, y: -1};
+            return {x: -1, y: -1};
         }
         else if (transform === "scale(1, -1)"){
-            pair = {x: 1, y: -1};
+            return {x: 1, y: -1};
         }
         else if (transform === "scale(-1, 1)"){
-            pair = {x: -1, y: 1};
+            return {x: -1, y: 1};
         }
         else {
-            pair = {x: 0, y: 0};
+            return {x: 0, y: 0};
         }
-        return pair;
     }
-    
-    return{
-        setPosition: setPosition,
-        getPosition: getPosition,
-        getOrientation: getOrientation,
-        translate: translate,
-        reflect: reflect,
-        reflectX: reflectX,
-        reflectY: reflectY,
-        createSprites: createSprites,
-        show: show,
-        hide: hide
-    };
-}())
 
-let tilemap = (function (){
-    function create(){
-        // create the tile map
-        for (let i = 0; i < TILEMAPSIZE; ++i){
-            let tile = document.createElement('div');
-            tile.className = "tile";
-            tile.id = "tile" + i;
-            document.getElementById("tilemap").appendChild(tile);
-        };            
+    setImage(index, imgSource) {
+        this.sprites[index].src = imgSource;
+    }
+
+    getZIndex(index) {
+        return parseInt(window.getComputedStyle(this.sprites[index]).zIndex);
+    }
+
+    setZIndex(index, position) {
+        this.sprites[index].style.zIndex = position;
+    }
+}
+
+class TileMap {
+    constructor(tilemapList){
+        this.tilemapDiv = document.querySelector('.tilemap');
         // set the number of columns
         let value = "";
         for (let i = 0; i < TILECOLUMNS; ++i){
             value += "auto ";
         }
-        document.getElementById("tilemap").style.gridTemplateColumns = value;
+        this.tilemapDiv.style.gridTemplateColumns = value;
+        
+        // create the tile map
+        this.tiles = [];
+        for (let i = 0; i < TILEMAPSIZE; ++i){
+            this.tiles.push(this.tilemapDiv.appendChild(document.createElement('div')));
+        };            
+        
         // insert images
-        let index = 0;
-        tilemapList.forEach(x => {
-            let img = document.createElement('img');
-            img.src = x;
-            img.className = "tileImage";
-            document.getElementById("tile" + index).appendChild(img);
-            ++index; 
+        tilemapList.forEach((src, i) => {
+            const img = document.createElement('img');
+            img.src = src;
+            this.tiles[i].appendChild(img);
         })
     }
 
-    function show() {
-        spriteStyle = document.getElementById("tilemap").style
-        spriteStyle.visibility = "visible";
+    show() {
+        this.tilemapDiv.style.visibility = "visible";
     }
 
-    function hide() {
-        spriteStyle = document.getElementById("tilemap").style
-        spriteStyle.visibility = "hidden";
-    }
-        
-    function translate(dX, dY) {
-        const posLeft = parseInt(document.getElementById("viewport").style.left.slice(0, -2)) + dX;
-        const posTop = parseInt(document.getElementById("viewport").style.top.slice(0, -2)) + dY;
-        document.getElementById("viewport").style.left = posLeft + "px";
-        document.getElementById("viewport").style.top = posTop + "px";
-    }
-    
-    function setPosition(dX, dY) {
-        document.getElementById("viewport").style.top = dY + "px";
-        document.getElementById("viewport").style.left = dX + "px";
+    hide() {
+        this.tilemapDiv.style.visibility = "hidden";
     }
 
-    function getPosition() {
+    translate(dX, dY) {
+        const { style } = this.tilemapDiv;
+        const posLeft = Number(style.left.slice(0, -2)) + dX;
+        const posTop = Number(style.top.slice(0, -2)) + dY;
+        style.left = posLeft + "px";
+        style.top = posTop + "px";
+    }
+
+    setPosition(dX, dY) {
+        const { style } = this.tilemapDiv;
+        style.top = dY + "px";
+        style.left = dX + "px";
+    }
+
+    getPosition() {
+        const { left, top } = this.tilemapDiv.style;
         return {
-            x: parseInt(document.getElementById("viewport").style.left.slice(0, -2)),
-            y: parseInt(document.getElementById("viewport").style.top.slice(0, -2))
+            x: left.slice(0, -2),
+            y: top.slice(0, -2)
         };
     }
-    
-    return{
-        setPosition: setPosition,
-        getPosition: getPosition,
-        translate: translate,
-        show: show,
-        hide: hide,
-        create: create};   
-}())
 
-initializeKeyBindings();
-
-tilemap.setPosition(0, 0);
-tilemap.create();
-
-sprites.createSprites(spriteSet);
-sprites.setPosition(0, 300, 300);
-sprites.show(0); 
-
-function initializeKeyBindings(){
-    document.addEventListener('keydown', (event) => {
-        if (event.key === 'ArrowUp') {
-            tilemap.translate(0, 10);
-        }
-        else if (event.key === 'ArrowDown') {
-            tilemap.translate(0, -10);
-        }
-        if (event.key === 'ArrowLeft') {
-            tilemap.translate(10, 0);
-        }
-        else if (event.key === 'ArrowRight') {
-           tilemap.translate(-10, 0);
-        }
-        if (event.key === 'w') {
-            sprites.translate(0, 0, -10);
-        }
-        if (event.key === 's') {
-            sprites.translate(0, 0, 10);
-        }
-        if (event.key === 'a') {
-            sprites.translate(0, -10, 0);
-        }
-        if (event.key === 'd') {
-            sprites.translate(0, 10, 0);
-        }
-        if (event.key === ' ') {
-            sprites.reflectY(0);
-        }
-        if (event.key === 'f') {
-            sprites.reflectX(0);
-        }
-    });
+    setImage(index, imgSource) {
+        this.tiles[index].children[0].src = imgSource;
+    }
 }
 
-function loadResources(){
-    const img1 = 'https://georgec0stanza.github.io/2DgraphicsEngine/images/ferns.jpg';
-    const img2 = 'https://georgec0stanza.github.io/2DgraphicsEngine/images/tulips.jpg';
-    const img3 = 'https://georgec0stanza.github.io/2DgraphicsEngine/images/water.jpg';
-    
-    const sprite0 = 'https://georgec0stanza.github.io/2DgraphicsEngine/images/daffodil.jpg';    
-    const sprite1 = 'https://georgec0stanza.github.io/2DgraphicsEngine/images/eagle.jpg';    
-
-    spriteSet = [sprite0, sprite1];
-    tileSet = [img1, img2, img3];
-    tilemapList = [tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0], tileSet[0] , tileSet[2], tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] , tileSet[2], tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] ,tileSet[2], tileSet[1] , tileSet[1] , tileSet[0] , tileSet[2]];    
-}
+export {SpritesMaker, TileMap};
---
+++ user.js
@@ -0,0 +1,81 @@
+import {SpritesMaker, TileMap} from 'main.js';
+'use strict';
+
+const { spriteSet, tilemapList } = loadResources();
+const tileMap = new TileMap(tilemapList);
+tileMap.setPosition(0, 0);
+
+const spriteController = new SpritesMaker(spriteSet);
+spriteController.setPosition(0, 235, 240);
+spriteController.show(0); 
+
+spriteController.setPosition(1, 235, 240);
+spriteController.show(1);
+
+let temp = spriteController.getZIndex(1) + 1;
+spriteController.setZIndex(0, temp);
+
+initializeKeyBindings();
+
+function loadResources(){
+    const img1 = 'images/ferns.jpg';
+    const img2 = 'images/tulips.jpg';
+    const img3 = 'images/water.jpg';
+
+    const sprite0 = 'images/daffodil.jpg';    
+    const sprite1 = 'images/eagle.jpg';    
+
+    const spriteSet = [sprite0, sprite1];
+    const tileSet = [img1, img2, img3];
+    const tilemapList = `
+    2 1 1 0 2 1 1 0 2 1 1 0 2 1 1 0 
+    2 1 1 0 2 1 1 0 2 1 1 0 2 1 1 0 
+    2 1 1 0 2 1 1 0 2 1 1 0 2 1 1 0 
+    2 1 1 0 0 2 2 1 1 0 2 1 1 0 2 1 
+    1 0 2 1 1 0 2 1 1 0 2 1 1 0 2 1 
+    1 0 2 1 1 0 2 1 1 0 2 1 1 0 2 1 
+    1 0 2 1 1 0 2 1 1 0 2 2 1 1 0 2 
+    1 1 0 2 1 1 0 2 1 1 0 2 1 1 0 2 
+    1 1 0 2 1 1 0 2 1 1 0 2 1 1 0 2 
+    1 1 0 2 1 1 0 2 1 1 0 2 1 1 0 2
+    `
+      .trim()
+      .split(/\s+/)
+      .map(index => tileSet[index]);
+    return { spriteSet, tilemapList };
+}
+
+function initializeKeyBindings(){
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'ArrowUp') {
+            tileMap.translate(0, 10);
+        }
+        else if (event.key === 'ArrowDown') {
+            tileMap.translate(0, -10);
+        }
+        if (event.key === 'ArrowLeft') {
+            tileMap.translate(10, 0);
+        }
+        else if (event.key === 'ArrowRight') {
+            tileMap.translate(-10, 0);
+        }
+        if (event.key === 'w') {
+            spriteController.translate(0, 0, -10);
+        }
+        if (event.key === 's') {
+            spriteController.translate(0, 0, 10);
+        }
+        if (event.key === 'a') {
+            spriteController.translate(0, -10, 0);
+        }
+        if (event.key === 'd') {
+            spriteController.translate(0, 10, 0);
+        }
+        if (event.key === ' ') {
+            spriteController.reflectY(0);
+        }
+        if (event.key === 'f') {
+            spriteController.reflectX(0);
+        }
+    });
+}
