86dc3542c6df678eb03492813e4db92d016e7287
David Harris <32494929+georgeC0stanza@users.noreply.github.com>
2020-05-12 21:21:00 -0600

    trying something

--- user.js
+++ user.js
@@ -27,7 +27,41 @@ worker.addEventListener('message', function(e) {
     console.log(e.data);
 });
   
-worker.postMessage(['Happy Birthday', spriteController, TileMap]);
+worker.postMessage('Happy Birthday');
+
+
+
+
+
+var keyState = {};
+
+window.addEventListener('keydown', function(e) {
+keyState[e.key] = true;
+}, true);
+
+window.addEventListener('keyup', function(e) {
+keyState[e.key] = false;
+}, true);
+
+function gameLoop() {
+if (keyState['n']) {
+    spriteController.translate(0, -10, 0);
+    spriteController.translate(1, -10, 0);
+    spriteController.reflect(0, true, false);
+    spriteController.reflect(1, true, false);
+}
+if (keyState['m']) {
+    spriteController.translate(0, 10, 0);
+    spriteController.translate(1, 10, 0);
+    spriteController.reflect(0, false, false);
+    spriteController.reflect(1, false, false);
+}
+setTimeout(gameLoop, 10);
+}
+
+gameLoop();
+
+
 
 
 function loadResources(){
