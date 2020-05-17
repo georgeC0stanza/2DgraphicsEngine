c8de1a6a5447d0f53c5465141d1bbad474fbbafd
David Harris <32494929+georgeC0stanza@users.noreply.github.com>
2020-05-16 21:36:57 -0600

    dancing plants

--- worker.js
+++ worker.js
@@ -2,7 +2,11 @@ self.addEventListener('message', function(e) {
     setInterval(() => {
         let rand = Math.random() * 20;
         let rand2 = Math.random() * 20;
-        self.postMessage([rand, rand2]);
+        let rand3 = Math.random();
+        let rand4 = Math.random() % 16;
+        let rand5 = Math.random() % 1;
+        
+        self.postMessage([rand, rand2,rand3, rand4, rand5]);
         
     }, 500);
 
