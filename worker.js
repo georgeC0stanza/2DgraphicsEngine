edb34b15444b99811aeb9cafc2fead2c4aa1107a
David Harris <32494929+georgeC0stanza@users.noreply.github.com>
2020-05-16 21:52:02 -0600

    msgnitude

--- worker.js
+++ worker.js
@@ -2,9 +2,9 @@ self.addEventListener('message', function(e) {
     setInterval(() => {
         let rand = Math.random() * 20;
         let rand2 = Math.random() * 20;
-        let rand3 = Math.random();
-        let rand4 = Math.random() % 16;
-        let rand5 = Math.random() % 1;
+        let rand3 = Math.random() * 10;
+        let rand4 = Math.random() * 10 % 16;
+        let rand5 = Math.random() * 10 % 1;
         
         self.postMessage([rand, rand2,rand3, rand4, rand5]);
         
